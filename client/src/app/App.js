a88af58cbe2d210078d8380b3f2959409d2caa6c
Dani <dvera1@babson.edu>
2022-06-25 11:26:22 -0400

    project set up, added packages, and folder structure

---
+++ client/src/app/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "../pages/home";
+
+function App() {
+  return (
+    <div class="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header, Button } from "semantic-ui-react";
+
+const home = () => {
+  return (
+    <div>
+      <Header as="h1">Landing Page Devsafio</Header>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default home;
