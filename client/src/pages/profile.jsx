034e72dbcf848d6f3795288dd9bafdbe8f5bc317
marciaalegria <ytasegundo@gmail.com>
2022-06-29 20:11:48 -0400

    crea rama perfil usuario

---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const profile = () => {
+  return (
+    <div>profile</div>
+  )
+}
+
+export default profile
+
+//foto perfil usuario
+
+//hola usuario
+
+
+//estado perfil
+
+//show the code realizados
+
+//stack principal
+
+
+//test técnicos
+
+
+//blog material apoyo
