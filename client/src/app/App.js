ac0e994cadca506216f0fdfc4905c9c5ce884839
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2022-07-01 19:43:55 -0400

    Revert "Login"

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,22 +1,9 @@
 import "./App.css";
 import Home from "../pages/home";
-import Login from "../pages/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+    <div class="App">
       <Home />
     </div>
   );
