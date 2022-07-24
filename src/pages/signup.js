fea4db51ec6f72ec012766db774dd6c49cfba402
David Torres <davidtorresim@gmail.com>
2022-07-24 18:46:25 -0400

    Signup con Axios

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,125 +1,103 @@
-import React from 'react';
-import { Formik, Field, ErrorMessage } from "formik";
+import React, {useState} from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
-import { Message} from 'semantic-ui-react'
+import axios from 'axios';
 import "./signup.css"
-// import axios from 'axios';
 
+const Signup = () => {
+	const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
 
-
-/* const CallTo = (email,password) => {
-    state = {
-        email: email,
-        password:password,
-      }
-
-      handleChange = event => {
-        this.setState({ name: event.target.value });
-      }
-    
-      handleSubmit = event => {
-        event.preventDefault();
-    
-        const user = {
-          name: this.state.name,
-          correo:this.state.correo
-        };
-    
-        axios.post(`https://jsonplaceholder.typicode.com/users`, { user })
-          .then(res => {
-            console.log(res);
-            console.log(res.data);
-          })
-      }
-      
-}  */
-
-
-const Signup = (props) => {
-
-    const {email, password,confirmPassword,onSubmitProp} = props;  /// esto lo recibo de algun lado o lo dejamos como props para que alguien los obtenga desde aca
-
-    return (
-          <Formik
-
-        onSubmit={(values, {setSubmitting,}) =>{
-            const timeOut = setTimeout(( )=>{
-                console.log(values);
-                onSubmitProp(values);
-                setSubmitting(false);
-                clearTimeout(timeOut);
-                console.log("pruebaonSubmit")
-            }, 1000);
-        }}
-          
-        initialValues={{ email:email, password:password,confirmPassword:confirmPassword,}}
-
-        validationSchema={Yup.object().shape({            
-            email: Yup.string()
-            .email("Correo no valido")
-            .min(3, "Faltan caracteres")
-            .required("Por favor, ingresa un correo electrónico válido"),
-            
-            password: Yup.string()
-            .equals([Yup.ref('confirmPassword'), null], "Las contraseñas no son iguales")
-            .min(8, "La clave debe contener más de 8 caracteres")
-            .required("Por favor ingrese una contraseña"),
-
-            confirmPassword: Yup.string()
-            .equals([Yup.ref('password'), null], "Las contraseñas no son iguales")
-            .min(8, "La clave debe contener más de 8 caracteres")
-            .required("Por favor ingrese la confirmación de la contraseña"),
-        })}>
-
-
-        {({ values,errors,touched,valid,handleSubmit}) =>{
-
-    
-
-        return (
-            <div>
-                <h1>Registro</h1>
-
-             
-             <div className='container'>
-             <form className='ui form' Method="post" onSubmit={handleSubmit}>
-                  <div className='field'>
-                    <label className="registertext" htmlFor="email">Correo Electrónico</label>
-                    <Field id='email' type="text" placeholder="Email" name='email'/>
-                    <ErrorMessage name="email">{(msg) => <p>{msg}</p>}</ErrorMessage>
-                    </div>
+	return (
+		<>
+			<Formik
+				initialValues={{
+					correo: '',
+                    password:'',
+                    confirmPassword:'',
+				}}
+				
+                validationSchema={Yup.object().shape({            
+                    correo: Yup.string()
+                    .email("Correo no valido")
+                    .min(3, "Faltan caracteres")
+                    .required("Por favor, ingresa un correo electrónico válido"),
                     
-                    <div className='field'>
-                    <label className="registertext" htmlFor="password">Contraseña</label>
-                    <Field  id='password' type="password" placeholder="Contraseña" name='password'/>
-                    {errors.password && touched.password && <p>{errors.password}</p>}
-                    </div>
+                    password: Yup.string()
+                    .equals([Yup.ref('confirmPassword'), null], "Las contraseñas no son iguales")
+                    .min(8, "La clave debe contener más de 8 caracteres")
+                    .required("Por favor ingrese una contraseña"),
+        
+                    confirmPassword: Yup.string()
+                    .equals([Yup.ref('password'), null], "Las contraseñas no son iguales")
+                    .min(8, "La clave debe contener más de 8 caracteres")
+                    .required("Por favor ingrese la confirmación de la contraseña"),
+                })}
+
+				onSubmit={(valores, {resetForm}) => {
+					resetForm();
+                      axios.post('/user', {
+                      correo: valores.correo,
+                      password: valores.password,
+                      })
+                      .then(function (response) {
+                        console.log(response);
+                      })
+                      .catch(function (error) {
+                        console.log(error);
+                      });
+
+					console.log('Formulario enviado');
+                    console.log(valores)
+					cambiarFormularioEnviado(true);
+					setTimeout(() => cambiarFormularioEnviado(false), 4000);
+				}}
+			>
+				{( {errors} ) => (
+					<Form className="formulario">
+                        <h1>Registro</h1>
+						<div>
+							<label htmlFor="correo">Correo</label>
+							<Field
+								type="text" 
+								id="correo" 
+								name="correo" 
+								placeholder="Correo" 
+							/>
+							<ErrorMessage name="correo" component={() => (<div className="error">{errors.correo}</div>)} />
+						</div>
+
+                        <div>
+                            <label htmlFor="password">Contraseña</label>
+                            <Field  
+                                type="password"
+                                id='password' 
+                                name='password' 
+                                placeholder="Contraseña" 
+                                /> 
+                            <ErrorMessage name="password" component={() => (<div className="error">{errors.password}</div>)} />
+						</div>
+
+                        <div>
+                            <label htmlFor="confirmPassword"> Confirmar Contraseña</label>
+                            <Field  
+                                type="password"
+                                id='confirmPassword' 
+                                name='confirmPassword' 
+                                placeholder="Confirmar Contraseña" 
+                                /> 
+                            <ErrorMessage name="password" component={() => (<div className="error">{errors.confirmPassword}</div>)} />
+						</div>
 
 
-                    <div className='field'>
-                    <label className="registertext" htmlFor="confirmPassword">Confirmar Contraseña</label>
-                    <Field  id='confirmPassword' type="password" placeholder="Confirmar Contraseña" name='confirmPassword'/>
-                    {errors.confirmPassword && touched.confirmPassword && <p>{errors.confirmPassword}</p>}
-                    </div>
-                                
-                    <Message success header='Form Completed'content="You're all signed up for the newsletter"/>
-                </form>
-
-                <button type="submit" disabled={Object.values(errors).length > 0} className="ui blue button">
-                    Registrarse
-                </button>
-             </div>
-                 
-
-                     
+						<button type="submit">Enviar</button>
+						{formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+					</Form>
+				)}
 
 
-            </div>
-        );
-        }}
-        </Formik>
-       
-    );
+			</Formik>
+		</>
+	);
 }
-
+ 
 export default Signup;
