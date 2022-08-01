599ebbfaef8600d7b9d07c1c9b4ca22b9844918c
Dani <dvera1@babson.edu>
2022-08-01 09:35:41 -0400

    Updated login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,95 +1,107 @@
 import React, { useState } from "react";
-import { Form, Button } from "semantic-ui-react";
-import { replace, useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import "../assets/styles.css";
 import Navbar from "../components/navbar";
-import { Link, useNavigate } from "react-router-dom";
+import { Button } from "semantic-ui-react";
+import { useNavigate, Link } from "react-router-dom";
 
-function Login() {
+const Login2 = () => {
   const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
+  //   const [errorRegistro, setErrorRegistro] = useState("");
   const navigate = useNavigate();
-  const formik = useFormik({
-    initialValues: {
-      // initial values for the form
-      email: "",
-      password: "",
-    },
-    validationSchema: Yup.object({
-      email: Yup.string() // validation for the email field
-        .email("Mail invalido")
-        .required("El email es requerido"),
-      password: Yup.string() // validation for the password field
-        .required("La contraseña es requerida")
-        .min(8, "La contraseña debe tener 8 caracteres minimo"),
-      /* .matches(
-          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-          "la contraseña debe tener al menos una letra mayuscula, una minuscula, un numero y un caracter especial"
-        ), */
-    }),
-
-    onSubmit: (forminfo, { resetForm }) => {
-      resetForm();
-      axios
-        .post("http://localhost:8080/api/1/users/signin/", {
-          email: forminfo.email,
-          password: forminfo.password,
-        })
-        .then(function (response) {
-          if (response.data.success === true) {
-            navigate("/dashboard", { replace: true });
-          }
-        })
-        .catch(function (error) {
-          alert(error.response.data.message);
-        });
-      console.log("Formulario enviado");
-      console.log(forminfo);
-      cambiarFormularioEnviado(true);
-      setTimeout(() => cambiarFormularioEnviado(false), 5000);
-    },
-  });
 
   return (
     <>
       <Navbar />
-      <div className="login-container">
-        <Form className="formulario" onSubmit={formik.handleSubmit}>
-          <h2 className="login-greeting">Ingresa</h2>
-          <div>
-            <Form.Input
-              placeholder="Correo"
-              type="email"
-              className="forminputemail"
-              name="email"
-              onChange={formik.handleChange}
-              error={formik.errors.emai}
-              value={formik.values.email}
-            ></Form.Input>
-          </div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string()
+            .email("Correo no valido")
+            .min(3, "Faltan caracteres")
+            .required("Por favor, ingresa un correo electrónico válido"),
 
-          <Form.Input
-            placeholder="Contraseña"
-            type="Password"
-            className="Form.Input.Password"
-            name="password"
-            onChange={formik.handleChange}
-            error={formik.errors.password}
-            value={formik.values.password}
-          ></Form.Input>
-          <Button primary type="submit" className="login-btn">
-            Iniciar Sesión
-          </Button>
-          <Link to="/dashboard">
-            <Button color="teal" style={{ marginTop: "40px" }}>
-              Link Temporal a Dashboard-Profile
+          password: Yup.string()
+            .min(8, "La clave debe contener al menos 8 caracteres")
+            .required("Por favor ingrese una contraseña"),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          resetForm();
+          axios
+            .post("http://localhost:8080/api/1/users/signin/", {
+              email: values.email,
+              password: values.password,
+            })
+            .then(function (response) {
+              if (response.data.success === true) {
+                navigate("/dashboard", { replace: true });
+              }
+            })
+            .catch(function (error) {
+              alert(error.response.data.message);
+              console.log("error:", error.response.data.message);
+            });
+
+          console.log("Formulario login enviado");
+          console.log("datos ingresados por usuario:", values);
+          cambiarFormularioEnviado(true);
+          setTimeout(() => cambiarFormularioEnviado(false), 5000);
+        }}
+      >
+        {({ errors }) => (
+          <Form className="formulario">
+            <h2 className="login-greeting">Ingresa</h2>
+            <div>
+              <Field
+                type="text"
+                id="email"
+                name="email"
+                placeholder="Correo"
+                autoComplete="off"
+              />
+              <ErrorMessage
+                name="email"
+                component={() => <div className="error">{errors.email}</div>}
+              />
+            </div>
+
+            <div>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Contraseña"
+              />
+              <ErrorMessage
+                name="password"
+                component={() => <div className="error">{errors.password}</div>}
+              />
+            </div>
+
+            <Button primary type="submit" className="login-btn">
+              Iniciar Sesión
             </Button>
-          </Link>
-        </Form>
-      </div>
+            <Link to="/dashboard">
+              <Button color="teal" style={{ marginTop: "40px" }}>
+                Link Temporal a Dashboard-Profile
+              </Button>
+            </Link>
+            {formularioEnviado && (
+              <div class="ui success message">
+                {" "}
+                <i class="close icon"></i>
+                <div className="header">Tu ingreso fue exitoso.</div>
+              </div>
+            )}
+          </Form>
+        )}
+      </Formik>
     </>
   );
-}
+};
 
-export default Login;
+export default Login2;
