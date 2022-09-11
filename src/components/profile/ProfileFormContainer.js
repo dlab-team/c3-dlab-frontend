8cf2bbdb89330047b602d899f985ba995734a793
Dani <dvera1@babson.edu>
2022-09-11 15:47:10 -0400

    Data is configured to be sent to end point add user. User id is hardcoded

--- src/components/profile/ProfileFormContainer.js
+++ src/components/profile/ProfileFormContainer.js
@@ -1,51 +1,36 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Formik, Form, setFieldValue } from "formik";
+import { Formik, Form } from "formik";
 import FormikControl from "./FormikControl";
 import CompetencyLevel from "./CompetencyLevel";
 import { Accordion, Icon, Container } from "semantic-ui-react";
-import {
-  initialValues,
-  frameworkList,
-  profileRadioOptions,
-  valuesFromDB,
-  validationSchema,
-  onSubmit,
-} from "./data";
+import { initialValues, profileRadioOptions } from "./data";
 import { FormDataContext } from "../../contexts/formDataContext";
 import axios from "axios";
 import "../../assets/styles.css";
 
 export default function ProfileFormContainer() {
-  const [profileValuesDB, setProfileValuesDB] = useState(null);
+  // const [profileValuesDB, setProfileValuesDB] = useState(null);
   const [isReady, setIsReady] = useState(false);
   // const { formData, formDataError, getAllFormData } =
   //   useContext(FormDataContext);
   const [frameworks, setFrameworks] = useState(null);
-  const [languages, setLanguages] = useState(null);
-  const [tools, setTools] = useState(null);
+  // const [languages, setLanguages] = useState(null);
+  // const [tools, setTools] = useState(null);
   const [formSubmitted, setFormSubmitted] = useState(false);
 
-  useEffect(() => {
-    setIsReady(false);
-  }, []);
-
   function handleResponse(response) {
+    console.log(response.data.res.frameworks);
     setFrameworks(response.data.res.frameworks);
     setIsReady(true);
   }
 
-  console.log("framewoksDB:", frameworks);
-
   const renderFrameworks = (initVals, setFieldValue) => {
-    // const sortedFrameworks = frameworks.sort((a, b) =>
-    //   a.name > b.name ? 1 : -1
-    // );
     return Object.entries(initVals)
       .slice(0, 2)
       .map((item, i) => {
-        console.log("initVals:", initVals);
-        console.log("item[1].name:", item[1].name);
-        console.log("i", i);
+        // console.log("initVals:", initVals);
+        // console.log("item[1].name:", item[1].name);
+        // console.log("i", i);
         return (
           <CompetencyLevel
             id={item[1].id}
@@ -63,30 +48,44 @@ export default function ProfileFormContainer() {
     return (
       <Formik
         initialValues={initialValues}
-        // onSubmit={async (values) => {
-        //   axios
-        //     .put("http://localhost:8080/api/1/profile/adduser", {
-        //       oracle: values.oracle,
-        //       cassandra: values.cassandra,
-        //     })
-        //     .then(function (response) {
-        //       alert(response);
-        //     })
-        //     .catch(function (error) {
-        //       alert(error.response.data.message);
-        //     });
-        //   console.log("Profile Form Sent");
-        //   console.log("ProfileFormValues:", values);
-        //   setFormSubmitted(true);
-        //   setTimeout(() => setFormSubmitted(false), 5000);
-        // }}
-        onSubmit={(values) => {
-          console.log("Form data", values);
-          console.log(
-            "Saved data stringify",
-            JSON.parse(JSON.stringify(values))
+        onSubmit={async (values) => {
+          setFormSubmitted(true);
+
+          const dataToSend = Object.entries(values).reduce(
+            (acumulador, elementoActual) => {
+              // ["react", { id: 1, level: 2}]
+              const _key = elementoActual[0]; // react
+              const values = elementoActual[1]; // { id: 1, level: 2}
+
+              const newObject = {
+                UserId: 1,
+                FrameworkId: values["id"],
+                level: values["level"],
+              };
+
+              acumulador.push(newObject);
+
+              return acumulador;
+            },
+            []
           );
-          console.log(Object.values(values));
+
+          console.log(dataToSend);
+          debugger;
+          axios
+            .put("http://localhost:8080/api/1/profile/adduser", {
+              frameworks: dataToSend,
+            })
+            .then(function (response) {
+              alert(response);
+              setFormSubmitted(false);
+            })
+            .catch(function (error) {
+              alert(error.response.data.message);
+              setFormSubmitted(false);
+            });
+          console.log("Profile Form Sent");
+          console.log("ProfileFormValues:", values);
         }}
         // validationSchema={validationSchema}
         // enableReinitialize
