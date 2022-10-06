dc42318522be0c4915c4f69616a432bf1f877fe9
marciaalegria <ytasegundo@gmail.com>
2022-10-06 20:57:46 -0300

    archivo api_constants para las rutas

--- src/constants/api_constants.js
+++ src/constants/api_constants.js
@@ -1,7 +1,15 @@
 const URL_BASE="http://localhost:8080/api"
 
 const url_api={
+    //get
     profile_form: URL_BASE + "/1/profile/form",
-    current_user: URL_BASE + "/users/1/currentuser"
+    current_user: URL_BASE + "/1/users/currentuser/",
+    //put
+    add_user: URL_BASE + "/1/profile/adduser",
+    //post
+    get_user: URL_BASE + "/1/profile/getuser",
+    sign_up: URL_BASE + "/1/users/signup",
+    sign_in: URL_BASE + "/1/users/signin",
+    sign_out: URL_BASE + "/users/1/signout"
 }
 export {url_api}
